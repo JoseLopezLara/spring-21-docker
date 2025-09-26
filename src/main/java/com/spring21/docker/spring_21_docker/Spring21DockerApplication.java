09faceafb18b2972a715443d4d146a20bcfd6e63
José López Lara <jose.lopez.lara@outlook.com>
2025-09-26 09:17:07 -0600

    First commit

---
+++ src/main/java/com/spring21/docker/spring_21_docker/Spring21DockerApplication.java
@@ -0,0 +1,13 @@
+package com.spring21.docker.spring_21_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring21DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring21DockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring21/docker/spring_21_docker/Spring21DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring21.docker.spring_21_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring21DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
